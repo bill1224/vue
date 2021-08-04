a9d4635f51fe162e140d0fc2223028b81406107e
jongin <bill1224@naver.com>
2021-08-04 23:29:14 +0900

    vue - Create / Read

---
+++ vue_study/src/components/Read.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <table>
+            <tr v-for="(value,i) in data" :key=i>
+                <td>{{ value.writer }}</td>
+                <td>{{ value.title }}</td>
+                <td>{{ value.content }}</td>
+            </tr>
+        </table>
+        <button @click="write">글쓰기</button>
+    </div>
+
+</template>
+
+<script>
+import data from '../data/index.js'
+export default {
+    name: 'Read',
+    data() {
+        return {
+            data: data
+        }
+    },
+    methods: {
+        write() {
+            this.$router.push({
+                path: 'create'
+            })
+        }
+    }
+}
+</script>
--- vue_study/src/router.js
+++ vue_study/src/router.js
@@ -1,21 +1,33 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "./views/Home";
+// import Home from "./views/Home";
 import About from "./views/About";
+import Read from "./components/Read";
+import Create from "./components/Create";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: "history",
     routes: [
-        { 
-            path:"/",
-            component: Home
-        },
+        // { 
+        //     path:"/",
+        //     component: Home
+        // },
         {
             path:"/about",
             component: About
-        }
+        },
+        {
+            path: "/",
+            name: "Read",
+            component: Read
+        },
+        {
+            path: "/create",
+            name: "Create",
+            component: Create
+        },
     ]
 
 });
