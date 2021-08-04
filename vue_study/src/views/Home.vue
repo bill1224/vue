40dd00a6ce116e8b2106528971e34ed882c76637
jongin <bill1224@naver.com>
2021-08-04 18:33:07 +0900

    data controll using v-model

---
+++ vue_study/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Welcome to {{ title }}!</h1>
+        <input type="text" v-model="input1" />
+        <button @click="getData">Get</button>
+        <button @click="setData">Set</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "초보 개발자", 
+            input1: ""           
+        };
+    },
+    methods: {
+        getData() {
+            //method에서 변수명에 접근할 때에는 this를 붙힌다.
+            alert(this.input1);
+        },
+        setData() {
+            this.input1 = "1234"
+        }
+    }
+};
+</script>
