7810b1d98dd9c8273f9c4a115347b6d37137664b
jongin <bill1224@naver.com>
2021-08-09 03:09:05 +0900

    복구

--- vue_laravel/routes/web.php
+++ vue_laravel/routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Models\User;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
-use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +19,9 @@ use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register')
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
@@ -70,7 +71,78 @@ Route::get('/home/{id}', function(Request $request) {
 //RouteServiceProvider.php의 boot()안에 pattern()를 사용하면, Route에서 따로 where로 지정하지 않아도,
 //항상 적용되는 포멧을 정의할 수 있다. 
 //Route::pattern('id', '[0-9]+'); 을 지정하면, 항상 id 파라미터를 받을 경우에는 해당 정규식이 적용이 된다.
-Route::get('user/{id}', function ($id) {
-    // Only executed if {id} is numeric...
-    return "User ID:".$id;
+// Route::get('user/{id}', function ($id) {
+//     // Only executed if {id} is numeric...
+//     return "User ID:".$id;
+// });
+
+//name 메소드를 체이닝 하여 라우트에 이름을 지정할 수 있습니다. 라우트 이름은 언제나 고유해야 합니다.
+// Route::get('user/profile', function () {
+//     return 'name메소드를 지정해서, route함수를 통해 리다이렉션을 생성할 때 route이름을 사용해서 왔다';
+// })->name('profile');
+
+//주어진 라우트에 대한 이름이 할당되면, 전역 route 함수를 통해서 URL 또는 리다이렉션을 생성할 때 라우트 이름을 사용할 수 있습니다.
+// Route::get('routeName', function () {
+//     return redirect()->route('profile');
+// });
+
+// Route::get('user/{id}/profile', function ($id) {
+//     return "user id :".$id;
+// })->name('profile');
+
+//route()함수에 파라미터도 같이 보낼 수 있으며, 배열형태로 여러개의 파라미터를 전달할 수도있다. 
+//이때, 기존 name()된 라우터에서 파라미터가 지정되어있지 않은  초과된 파라미터는 
+//URL에 쿼리문형태로 추가되어진다. /user/1/profile?photos=yes
+Route::get('routeName', function () {
+    return redirect()->route('profile', ['id' => 1, 'photos' => 'yes']);
+});
+
+//라우트 그룹
+//그룹을 사용하면, 라우트마다 미들웨어를 지정해주지 않아도 된다. 
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+
+//     Route::get('user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });
+
+//prefix를 이용하면, url접두사가 되는 것을 지정해줄 수 있다.
+// Route::prefix('admin')->group(function () {
+//     //admin/users
+//     Route::get('users', function () {
+//         return "USERS";
+//     });
+//     //admin/group
+//     Route::get('group', function () {
+//         return "GROUP";
+//     });
+// });
+
+//name(".")을 사용하면, route의 name()을 지정할때 해당 접두사를 지정해줄 수 있다.
+// Route::name('admin.')->group(function () {
+//     Route::get('users', function () {
+//         return "USERS";
+//     })->name('users'); // = name('admin.users')
+
+//     Route::get('group', function () {
+//         return "GROUP";
+//     })->name('group'); = name('admin.group')
+// });
+
+// Route::get('abc', function() {
+//     return redirect()->route('admin.group');
+// });
+
+// request URI 로 부터 일치하는 ID 값을 가진 모델 인스턴스를 주입할것입니다. 
+//만약 데이터베이스에서 매칭되는 모델 인스턴스를 찾을 수 없으면, 자동으로 404 HTTP response 가 생성됩니다.
+Route::get('users/{user}', function (User $user) {
+    return $user->email;
+});
+
+//id값이 아니라 다른 컬럼으로 사용하길 원한다면, 지정해줄 수도 있다.
+Route::get('api/posts/{post:slug}', function (App\Post $post) {
+    return $post;
 });
