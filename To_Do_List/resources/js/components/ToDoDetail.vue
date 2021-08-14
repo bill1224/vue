eb657737d973bb89bdc0b7677fb6a3968e47c261
bill1224 <bill1224@naver.com>
2021-08-14 20:34:15 +0900

    style binding -> hidden to over text

--- To_Do_List/resources/js/components/ToDoDetail.vue
+++ To_Do_List/resources/js/components/ToDoDetail.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="container">
-        <div class="text-center fs-1 mb-4">To Do Detail</div>
-        <div class="row border border-3 rounded-1 mb-2 p-4">
-            <div class="col">{{ toDoDetail.title }}</div>
-            <div class="col">{{ toDoDetail.created_at }}</div>
+    <div class="container w-3/5">
+        <div class="text-center fs-1 mb-4" @click="redirectToHome">To Do Detail</div>        
+        <div class="grid grid-cols-6 gap-4 border-3 rounded-1 mb-2 p-4 w-full">
+            <div class="col-start-1 col-end-6" :style="hiddenText">{{ toDo.title }}</div>
+            <div class="col-start-7">
+                <div class="grid grid-cols-2 text-sm">
+                    <div>CreatedAt</div>
+                    <div> {{ String(toDo.created_at).slice(0, 10) }}</div>
+                </div>
+                <div class="grid grid-cols-2 text-sm">
+                    <div>DeadLine</div>
+                    <div>{{ checkDeadLine(toDo.deadline) }}</div>
+                </div>
+            </div>            
         </div>
-        <div class="row border border-3 border-primary rounded-1 mb-2">
-            <div class="col-8">col-8</div>
-            <div class="col-4">col-4</div>
+        <div class="border border-3 border-primary rounded-1 mb-2 p-4">
+            <div class="col-8">{{ toDo.description !== null ? toDo.description : "상세내용을 적어주세요."}}</div>            
         </div>
         <div class="text-center">
-            <button type="button" class="btn btn-warning mr-2">Warning</button>            
-            <button type="button" class="btn btn-primary" @click="redirectToDateSet(toDoDetail.id)">상세설정</button>         
+            <button type="button" class="btn btn-warning mr-2" @click="complete(toDo.id)">완료</button>            
+            <button type="button" class="btn btn-primary" @click="redirectToDateSet(toDo.id)">상세설정</button>         
         </div>                                            
     </div>
 </template>
@@ -23,15 +31,58 @@ export default {
     }),
 
     props: {
-        toDoDetail: {
-            type: Array,
+        toDoId: {
+            type: Number,
             required: true,
         }
+    },
+
+    data() {
+        return {
+            id: this.toDoId,
+            toDo: [],
+            hiddenText: {
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap'
+            }            
+        }
+    },
+
+    created() {
+        axios.get('../../api/todo/Showdetail', {
+            params: {
+                id: this.id
+            }
+        }).then(res => {
+            this.toDo = res.data.todo_detail;
+            console.log(res);
+        });
     },
 
     methods: {
         redirectToDateSet(id) {
             window.location.href = `/todo/setDetail/${id}`;
+        },
+
+        checkDeadLine(deadline) {
+            return deadline !== null ? deadline : "undefined";
+        },
+
+        complete(id) {
+          axios.get('../../api/todo/complete', {
+              params: {
+                  ToDoId: id
+              }
+          }).then(res => {
+                console.log(res);
+                // this.ToDoList = res.data.list_arr;             
+        });
+        window.location.href = '/';
+      },
+
+      redirectToHome() {
+            window.location.href = '/';
         }
     }
 }
--- To_Do_List/resources/js/components/ToDoView.vue
+++ To_Do_List/resources/js/components/ToDoView.vue
@@ -3,9 +3,9 @@
         @click="onClickToDetail(toDo.id)"
         class="text-center border-2 rounded hover:bg-gray-400 mb-1"     
     >
-        <p class="text-start p-2">{{ toDo.title }}</p>
+        <p class="text-start p-2" :style="hiddenText">{{ toDo.title }}</p>
         <!-- <span>{{ toDo.deadline !== null ? toDo.deadline : undefined_deadline }}</span> -->
-        <p class="text-end"> {{ toDo.deadline !== null  ? `D - ${elapsedDay}` :  "시간설정을 해주세요."}}</p>
+        <p class="text-end p-2"> {{ toDo.deadline !== null  ? `D - ${elapsedDay}` :  "시간설정을 해주세요."}}</p>
     </div>
 </template>
 
@@ -29,6 +29,11 @@ export default {
             today: new Date(today[0], today[1], today[2]).getTime(),
             deadline: new Date(deadline[0], deadline[1], deadline[2]).getTime(),
             elapsedDay: '',
+            hiddenText: {
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap'
+            }
         }
     },
 
