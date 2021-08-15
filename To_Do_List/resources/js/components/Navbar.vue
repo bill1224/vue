6e26c29eea771194f2fc5240ddda5550c6d7080e
bill1224 <bill1224@naver.com>
2021-08-16 01:30:14 +0900

    group filtering

--- To_Do_List/resources/js/components/Navbar.vue
+++ To_Do_List/resources/js/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="mb-2">
+    <div class="mb-2" @click="changeCategory(1004)">
         일반
     </div>
-    <div class="mb-2">
+    <div class="mb-2" @click="changeCategory(1141)">
         중요
     </div>
     <!-- <div 
@@ -14,16 +14,15 @@
     </div> -->
 
     <div class="form-floating">
-        <select class="form-select" id="floatingSelect" aria-label="Floating label select example" v-model="value">
+        <select class="form-select" id="floatingSelect" aria-label="Floating label select example" v-model="categoryNum">
             <template v-for="Group in groupArr" :key="Group.id">            
                 <option :value="Group.id">{{ Group.group_name }}</option>
-            </template>
-            <!-- <option selected>Click this select menu</option> -->            
+            </template>                       
         </select>
         <label for="floatingSelect">Group with selects</label>
     </div>
     <div>
-        {{ value }}
+        {{ categoryNum }}        
     </div>
 </template>
 
@@ -37,7 +36,14 @@ export default {
     
     data() {
         return {     
-            value: '',   
+            categoryNum: '',
+            text: '변경전',   
+        }
+    },
+
+    watch: {
+        categoryNum: function (newVal, oldVal) {
+            this.$emit("getCategoryNumber", newVal);
         }
     },
 
@@ -45,6 +51,12 @@ export default {
     //     axios.get('api/getGroup').then(res => {
     //         this.Groups = res.data.Groups;
     //     })
-    // }
+    // },
+
+    methods: {
+        changeCategory(Num) {
+            this.categoryNum = Num
+        }
+    },
 }
 </script>
--- To_Do_List/resources/js/components/ToDoList.vue
+++ To_Do_List/resources/js/components/ToDoList.vue
@@ -3,14 +3,14 @@
 
     <div class="flex h-full">
         <div class="w-1/5 border-r-2 border-solid border-gray-600">
-            <Navbar :group-arr="Groups"/>
+            <Navbar :group-arr="Groups" @get-category-number="getCategoryNumber"/>
         </div>
 
         <div class="w-4/5 flex flex-col px-16">
             <div class="text-center mb-4"><span class="fs-1">To Do List of Jong-In</span></div>
-            <div v-if="currentState === '0'">해야할 일 : {{ NotCompleteToDOList.length }} </div>
+            <!-- <div v-if="currentState === '0'">해야할 일 : {{ NotCompleteToDOList.length }} </div>
             <div v-else-if="currentState === '1'">완료한 일 : {{ NotCompleteToDOList.length }}</div>
-            <div v-else>전체 : {{ NotCompleteToDOList.length }}</div>
+            <div v-else>전체 : {{ NotCompleteToDOList.length }}</div> -->
 
             <div class="flex-initial p-2">
                 <input 
@@ -30,7 +30,10 @@
                 <button type="button" class="btn btn-warning" @click="changeState('0')">할 일</button>            
                 <button type="button" class="btn btn-primary mx-2" @click="changeState('1')">완료</button>      
                 <button type="button" class="btn btn-primary" @click="changeState('all')">전체</button>      
-            </div>         
+            </div>
+            <div>
+                {{ value }}    
+            </div>     
         </div>
     </div>
     
@@ -51,11 +54,12 @@ export default {
     },
 
     data() {
-        return {
+        return {            
             ToDoList: [],
             title: '',
             currentState: "0",
-            Groups: '',       
+            Groups: '',
+            value: 1004,            
         }
     },
 
@@ -72,8 +76,14 @@ export default {
 
     computed: {
         NotCompleteToDOList() {
-            return this.ToDoList.filter(todo => this.currentState === "all" || todo.completion_is === this.currentState);
-        }
+            if(this.value === 1004) {
+                return this.ToDoList.filter(todo => this.currentState === "all" || todo.completion_is === this.currentState);
+            } else if (this.value === 1141) {
+                return this.ToDoList.filter(todo => this.currentState === "all" || todo.important_is === 1);
+            } else {
+                return this.ToDoList.filter(todo => this.currentState === "all" || todo.group === String(this.value));
+            }
+        }          
     },
 
     methods:{
@@ -111,6 +121,10 @@ export default {
       changeState(value) {
           this.currentState = value;
           return;
+      },
+
+      getCategoryNumber(value) {
+          this.value = value
       }
     }
 }
