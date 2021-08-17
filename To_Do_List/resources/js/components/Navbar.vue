e503b9274db4ee38d5b7bc0378d592a7135a9eb3
bill1224 <bill1224@naver.com>
2021-08-17 17:10:22 +0900

    when click to empty group list -> alert

--- To_Do_List/resources/js/components/Navbar.vue
+++ To_Do_List/resources/js/components/Navbar.vue
@@ -3,7 +3,7 @@
         <div class="mb-2 border-b-2 p-2" @click="changeCategory(1004)" ref="getData1004">全体</div>
         <div class="mb-2 p-2" @click="changeCategory(1141)" ref="getData1141">重要</div>
         <div class="form-floating">
-            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" @change="changeCategory(categoryNum)" v-model="categoryNum">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" @change="changeCategory(categoryNum)" v-model="categoryNum" @click="Emptyalert">
                 <template v-for="Group in groupArr" :key="Group.id">            
                     <option :value="Group.id" :ref="`getData${Group.id}`">{{ Group.group_name }}</option>
                 </template>                       
@@ -14,7 +14,7 @@
             <div class="flex-initial">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512"><g fill="#1138f7"><circle cx="256" cy="256" r="236.17"/><path d="M256 512C114.853 512 0 397.167 0 256 0 114.853 114.853 0 256 0c141.167 0 256 114.853 256 256 0 141.167-114.833 256-256 256zm0-472.341C136.705 39.659 39.659 136.705 39.659 256S136.705 472.341 256 472.341 472.341 375.275 472.341 256c0-119.295-97.046-216.341-216.341-216.341z"/></g><g fill="#fff"><path d="M256 373.193c-10.946 0-19.83-8.864-19.83-19.83V155.067c0-10.946 8.884-19.83 19.83-19.83 10.946 0 19.83 8.884 19.83 19.83v198.296c0 10.966-8.884 19.83-19.83 19.83z"/><path d="M355.148 274.045H156.852c-10.946 0-19.83-8.884-19.83-19.83 0-10.946 8.884-19.83 19.83-19.83h198.296c10.966 0 19.83 8.884 19.83 19.83 0 10.946-8.864 19.83-19.83 19.83z"/></g></svg>                
             </div>            
-            <div class="ml-2">グループ作り</div> 
+            <div class="ml-2">グループ作り</div>
         </div>
     </div>
 </template>
@@ -44,9 +44,12 @@ export default {
             this.$emit('showModal');
         },
         
-        test(categoryNum) {
-            console.log(categoryNum);
-        }
+        Emptyalert() {
+            if( this.groupArr.length === 0) {
+                alert('グループを作ってください。');
+                return; 
+            }
+        },
     },
 }
 </script>
