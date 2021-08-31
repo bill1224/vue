545fe13cc6d7c3fae432cde8add42ec453e05b22
bill1224 <bill1224@naver.com>
2021-08-31 20:37:31 +0900

    Databases Query

--- vue_study/app/Http/Controllers/DatabaseController.php
+++ vue_study/app/Http/Controllers/DatabaseController.php
@@ -143,6 +143,201 @@ class DatabaseController extends Controller
         //             ->whereIn('id', [1, 2, 3])
         //             ->get();
 
+        //whereNotIn 주어진 배열안에 안속하는지
+        // $users = DB::table('users')
+        //             ->whereNotIn('id', [1, 2, 3])
+        //             ->get();
+
+        //whereNull 속성이 null값인지
+        // $users = DB::table('users')
+        //             ->whereNull('updated_at')
+        //             ->get();
+
+        //whereNotNull null이 아닌지
+        // $users = DB::table('users')
+        //             ->whereNotNull('updated_at')
+        //             ->get();
+
+        // whereDate 메소드는 컬럼의 값이 date 값인지 비교하는데 사용됩니다.
+
+        // $users = DB::table('users')
+        //                 ->whereDate('created_at', '2016-12-31')
+        //                 ->get();
+        // whereMonth 메소드는 컬럼의 값이 한해의 지정된 달과 같은지 비교하는데 사용됩니다.
+
+        // $users = DB::table('users')
+        //                 ->whereMonth('created_at', '12')
+        //                 ->get();
+        // whereDay 메소드는 컬럼의 값이 한달의 지정된 일과 같은지 비교하는데 사용됩니다.
+
+        // $users = DB::table('users')
+        //                 ->whereDay('created_at', '31')
+        //                 ->get();
+
+        // whereYear 메소드는 컬럼의 값이 지정된 년도와 같은지 비교하는데 사용됩니다.
+
+        // $users = DB::table('users')
+        //                 ->whereYear('created_at', '2016')
+        //                 ->get();
+        // whereTime 메소드는 컬럼의 값을 특정 시간과 비교하는데 사용할 수 있습니다.
+
+        // $users = DB::table('users')
+        //                 ->whereTime('created_at', '=', '11:20:45')
+        //                 ->get();
+        // whereColumn / orWhereColumn
+
+        // whereColumn 메소드는 두개의 컬럼이 동일하는 것을 확인하는데 사용할 수 있습니다.
+
+        // $users = DB::table('users')
+        //                 ->whereColumn('first_name', 'last_name')
+        //                 ->get();
+        // 또한 비교 연산자를 메소드에 전달할 수도 있습니다.
+
+        // $users = DB::table('users')
+        //                 ->whereColumn('updated_at', '>', 'created_at')
+        //                 ->get();
+        // whereColumn메소드는 또한 다수의 조건 배열을 전달 받을 수도 있습니다. 이 조건들은 and 연산자를 사용하여 연결됩니다.
+
+        // $users = DB::table('users')
+        //                 ->whereColumn([
+        //                     ['first_name', '=', 'last_name'],
+        //                     ['updated_at', '>', 'created_at'],
+        //                 ])->get();
+
+
+        //괄호 안에 제약조건을 그룹으로 묶는것이 가능
+        // $users = DB::table('users')
+        //    ->where('name', '=', 'John')
+        //    ->where(function ($query) {
+        //        $query->where('votes', '>', 100)
+        //              ->orWhere('title', '=', 'Admin');
+        //    })
+        //    ->get();
+        //select * from users where name = 'John' and (votes > 100 or title = 'Admin') 위의 코드는 이것과 같은 동작
+
+
+        //where exists를 사용 
+        // $users = DB::table('users')
+        //    ->whereExists(function ($query) {
+        //        $query->select(DB::raw(1))
+        //              ->from('orders')
+        //              ->whereRaw('orders.user_id = users.id');
+        //    })
+        //    ->get();
+
+        //select * from users
+            // where exists (
+            //     select 1 from orders where orders.user_id = users.id
+            // ) 위의 코드는 이것과 같음 
+
+        
+        //JSON where절 
+        // $users = DB::table('users')
+        //     ->whereJsonContains('options->languages', 'en')
+        //     ->get();
+
+
+        //orderBy 원하는 만큼 추가가능
+        // $users = DB::table('users')
+        //         ->orderBy('name', 'desc')
+        //         ->orderBy('email', 'asc')
+        //         ->get();
+
+        // latest 와 oldest 메소드는 여러준이 손쉽게 날짜를 기반으로 결과를 정렬
+        // created_at 컬럼을 기준으로 정렬됩니다
+        // $user = DB::table('users')
+        //         ->latest()
+        //         ->first();
+
+        //랜덤하게 불러오고 싶은경우 
+        // $randomUser = DB::table('users')
+        //         ->inRandomOrder()
+        //         ->first();
+
+        //reorder를 사용하면 기존의 정렬을 초기화, 새로 정렬을 해줄 수 있음
+        // $query = DB::table('users')->orderBy('name');
+        // $usersOrderedByEmail = $query->reorder('email', 'desc')->get();
+
+        
+        //groupBy / having
+        // groupBy 메소드에 여러개의 인자를 전달 할 수 있습니다
+        // 복잡한 having은 havingRaw를 사용
+        // $users = DB::table('users')
+        //         ->groupBy('account_id')
+        //         ->having('account_id', '>', 100)
+        //         ->get();
+
+
+        //skip과 take
+        //skip은  주어진 개수만큼 결과를 건너뛰기 위해서
+        //take는 반환되는 결과의 개수를 제한
+        // $users = DB::table('users')->skip(10)->take(5)->get();
+
+        //대신에, limit 과 offset 메소드를 사용할 수도 있습니다.
+        // $users = DB::table('users')
+        //         ->offset(10)
+        //         ->limit(5)
+        //         ->get();
+
+        //현재의 요청에서 주어진 입력값이 존재할 때에만 where 구문을 적용하고 싶을 수도 있습니다. 이 경우 when 메소드를 사용할 수 있습니다.
+        //when 메소드는 첫번째 파라미터가 true 일때 주어진 클로저를 실행합니다. 첫번째 파라미터가 false 라면 클로저는 실행되지 않을 것입니다.
+        //when 메소드의 세번째 파라미터로 또다른 클로저를 전달할 수 있습니다. 이 클로저는 첫번째 파라미터가 false 일때 실행됩니다
+        // $sortBy = null
+        // $users = DB::table('users')
+        //         ->when($sortBy, function ($query, $sortBy) {
+        //             return $query->orderBy($sortBy);
+        //         }, function ($query) {
+        //             return $query->orderBy('name');
+        //         })
+        //         ->get();
+
+
+        //insert
+        // DB::table('users')->insert(
+        //     ['email' => 'john@example.com', 'votes' => 0]
+        // );
+
+        // insertGetId 메소드를 사용하여 레코드를 추가하고, 추가된 ID를 획득할 수 있습니다.
+        // $id = DB::table('users')->insertGetId(
+        //     ['email' => 'john@example.com', 'votes' => 0]
+        // );
+
+        //Update
+        // $affected = DB::table('users')
+        //       ->where('id', 1)
+        //       ->update(['votes' => 1]);
+
+        //updateOrInsert 메소드는 일치하는 레코드가 없는 경우 레코드를 만들 수도 있습니다. 
+        //updateOrInsert 메소드는 첫 번째 인자의 컬럼과 값의 쌍을 사용하여 일치하는 데이터베이스 레코드를 찾습니다. 
+        // 레코드가 있으면 두 번째 인수의 값으로 업데이트됩니다. 레코드를 찾을 수 없으면 두 레코드의 병합 된 특성과 함께 새 레코드가 삽입됩니다.
+        // DB::table('users')
+        // ->updateOrInsert(
+        //     ['email' => 'john@example.com', 'name' => 'John'],
+        //     ['votes' => '2']
+        // );
+
+
+        //증가 / 감소
+        //두번째 인자는 증감될 단위 
+        // DB::table('users')->increment('votes');
+        // DB::table('users')->increment('votes', 5);
+        // DB::table('users')->decrement('votes');
+        // DB::table('users')->decrement('votes', 5);  
+
+        // 또한 이 작업을 수행동안 업데이트 되어야할 컬럼을 추가적으로 지정할 수도 있습니다.
+        // DB::table('users')->increment('votes', 1, ['name' => 'John']);
+
+        //delete
+        // DB::table('users')->delete();
+        // DB::table('users')->where('votes', '>', 100)->delete();
+
+        //truncate는 모든레코드를 삭제시키고 id값도 0으로 초기화시켜줍니다.
+        // DB::table('databases')->truncate();
+
+        //쿼리를 작성하는 동안 쿼리 바인딩과 SQL을 출력하기 위해 dd 또는 dump 메소드를 사용 할 수 있습니다.
+        //dd 메소드는 디버그 정보를 표시하고 요청 실행을 중단합니다. dump 메소드는 디버그 정보를 보여 주지만 요청이 계속 실행되도록합니다.
+        // DB::table('databases')->where('number', '<', 5)->dd(); //"select * from `databases` where `number` < ?"
+        // DB::table('databases')->where('number', '<', 5)->dump(); //"select * from `databases` where `number` < ?"
 
         return view('Database', [
             'Datas' => $Datas,
